fffacb01be7703c72812202ae6843d68c8c99d33
radmilababic <radaababic@gmail.com>
2023-01-22 10:40:12 +0100

    Kreirane model klase i migracije

---
+++ database/migrations/2023_01_22_142853_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('count');
+            $table->decimal('unit_price');
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
