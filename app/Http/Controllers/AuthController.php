d82389fb18fe89a26ed92f6bcf17400e375fce2f
radmilababic <radaababic@gmail.com>
2023-01-22 10:59:50 +0100

    Dodate putanje za autentifikaciju

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(["error" => "Login failed"], 400);
+        }
+        return response()->json([
+            "user" => $user,
+            "token" => $user->createToken($request->email)->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            return response()->json(["error" => "User already exists"], 400);
+        }
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+        return response()->json([
+            "user" => $user,
+            "token" => $user->createToken($request->email)->plainTextToken
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
 Route::get('orders', [OrderController::class, 'index']);
 Route::get('products', [ProductController::class, 'index']);
 Route::get('orders/{id}', [OrderController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
